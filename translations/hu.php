cdda4eaa0f66f1b48b024308439756aeba75f88a
rrd <rrd@1108.cc>
2020-12-26 10:56:53 +0100

    translation added

---
+++ translations/hu.php
@@ -0,0 +1,26 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{utanvetellenor}prestashop>utanvetellenor_7190b83ab2e460deb435b2e98bee7bfa'] = 'Leellenőrizzük, hogy a vásárlónak megjelenítsük-e az utánvételes fizetési opciót';
+$_MODULE['<{utanvetellenor}prestashop>utanvetellenor_97579d9c62bcdc9dc0732c1d3e82196f'] = 'Csomag visszautasítva';
+$_MODULE['<{utanvetellenor}prestashop>utanvetellenor_f4f70727dc34561dfde1a3c529b6205c'] = 'Beállítások';
+$_MODULE['<{utanvetellenor}prestashop>utanvetellenor_cebd5bbe0ffdecc270a8a324e5a277dd'] = 'Éles üzemmód';
+$_MODULE['<{utanvetellenor}prestashop>utanvetellenor_0fcab462cc2c95d8301be4c402581cb9'] = 'Éles vagy teszt üzemmód';
+$_MODULE['<{utanvetellenor}prestashop>utanvetellenor_00d23a76e43b46dae9ec7aa9dcbebb32'] = 'Bekapcsolva';
+$_MODULE['<{utanvetellenor}prestashop>utanvetellenor_b9f5c797ebbf55adccdd8539a65a0241'] = 'Kikapcsolva';
+$_MODULE['<{utanvetellenor}prestashop>utanvetellenor_b1f19188840162e6193269ed18a6e805'] = 'Public API kulcs amit a  https://utanvet-ellenor.hu/ oldalon hoztál létre';
+$_MODULE['<{utanvetellenor}prestashop>utanvetellenor_f319972897d9f82fffd0dc01967aa63c'] = 'Nyilvános API kulcs';
+$_MODULE['<{utanvetellenor}prestashop>utanvetellenor_51f472146ce2e8b3cbcbd80ecbf9a52b'] = 'Private API kulcs amit a  https://utanvet-ellenor.hu/ oldalon hoztál létre';
+$_MODULE['<{utanvetellenor}prestashop>utanvetellenor_f2aada2477160fe32464427808067a7c'] = 'Titkos API kulcs';
+$_MODULE['<{utanvetellenor}prestashop>utanvetellenor_6f0a4d964f69fd3564e450779c4466bd'] = 'A következő képlet szerint számítjuk: (jó-rossz) / összes, így a 0,5-ös megbízhatóság például 6 átvett és 2 át nem vett csomagot jelenthet.';
+$_MODULE['<{utanvetellenor}prestashop>utanvetellenor_3fdb2f733b09875a918a7163110a8bd4'] = 'Megbízhatósági mutató';
+$_MODULE['<{utanvetellenor}prestashop>utanvetellenor_a24b4af7ff274099986786055dd89449'] = 'Átvéve - erre állítjuk a rendelés státuszát';
+$_MODULE['<{utanvetellenor}prestashop>utanvetellenor_7f0f2c22ffdde19f3b35eb966ae19f4c'] = 'Az átvett csomagok rendelési státusza';
+$_MODULE['<{utanvetellenor}prestashop>utanvetellenor_c9cc8cce247e49bae79f15173ce97354'] = 'Mentés';
+$_MODULE['<{utanvetellenor}prestashop>configure_6ba60dfdc0e78d31e9a7b5f4292e0eef'] = 'Leellenőrizzük, hogy a vásárló megbízható-e az utánvételes fizetéshez';
+$_MODULE['<{utanvetellenor}prestashop>configure_659d2a98161063c3a80445f44ff09e61'] = 'Azok a vásárlók akik a megadott megbízhatósági küszöb alatt vannak nem fgnak tudni utánvétellel rendelni, ez a fizetési opció nem jelenik meg a számukra.';
+$_MODULE['<{utanvetellenor}prestashop>configure_805fd824e1c5f482dac21da3d4f2a7d7'] = 'Ez a modul abban segít, hogy minimalizáld az át nem vett csomagok számát.';
+$_MODULE['<{utanvetellenor}prestashop>configure_6da8e67225fdcfa78c3ea5dc3154b849'] = 'Felülírás';
+$_MODULE['<{utanvetellenor}prestashop>configure_5edf2f04cf0b21ff3a2c21647a1abf61'] = 'A felülírás telepítése sikeres.';
+$_MODULE['<{utanvetellenor}prestashop>configure_3d3fd387b06597fb8d6464d160589a3c'] = 'HIBA: A felülírás nincs telepítve';
